df019f0bbed845fa1262c8505b238f6d81312509
Louis Guillotin <louisguillotin.lg@gmail.com>
2020-12-18 14:41:47 +0100

    Refacto + Q5 et Q6

--- AgentRandom.py
+++ AgentRandom.py
@@ -1,7 +1,36 @@
+from Network import Network
+import random
+import numpy
+import torch
+
 class RandomAgent(object):
     """The world's simplest agent!"""
-    def __init__(self, action_space):
+    def __init__(self, action_space, ob_space):
         self.action_space = action_space
 
-    def act(self, observation, reward, done):
-        return self.action_space.sample()
+        self.network = Network(ob_space.shape[0], [16, 32 , 16], action_space.n)
+        self.t_network = Network(ob_space.shape[0], [16, 32 , 16], action_space.n)
+
+    def act(self, observation, epsilon):
+        # Convert to tensor
+        ob = torch.from_numpy(observation).float().unsqueeze(0)
+        # Switch to "evaluate mode"
+        self.network.eval()
+
+        # With no gradient
+        with torch.no_grad():
+            # Calc Q Val for observation
+            qval = self.network(ob)
+
+        # Switch to "training mode"
+        self.network.train()   
+
+        #Greedy
+        # Draw random in [0,1]
+        rand = random.random() 
+        if rand < (1 - epsilon):
+            # Do best action
+            return numpy.argmax(qval.data.numpy())
+        else:
+            # Do random action : choose action in [0, |action_space|]
+            return random.choice(numpy.arange(self.action_space.n))
