a5d89750a7572246f0882c8e212955c41ee75538
Louis Guillotin <louisguillotin.lg@gmail.com>
2020-12-27 18:47:53 +0100

    Tentative de fix Q7 et Q8

--- AgentRandom.py
+++ AgentRandom.py
@@ -2,35 +2,77 @@ from Network import Network
 import random
 import numpy
 import torch
+import math
 
 class RandomAgent(object):
     """The world's simplest agent!"""
-    def __init__(self, action_space, ob_space):
+    def __init__(self, action_space, ob_space, eta):
         self.action_space = action_space
+        self.eta = eta
+        self.steps_done = 0
 
-        self.network = Network(ob_space.shape[0], [16, 32 , 16], action_space.n)
-        self.t_network = Network(ob_space.shape[0], [16, 32 , 16], action_space.n)
+        self.net = Network(ob_space.shape[0], [16, 32 , 16], action_space.n)
+        self.target_net = Network(ob_space.shape[0], [16, 32 , 16], action_space.n)
 
-    def act(self, observation, epsilon):
+        self.optimizer = torch.optim.Adam(self.net.parameters(), lr=eta)
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def act(self, observation, EPS_END, EPS_START, EPS_DECAY):
+        eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * self.steps_done / EPS_DECAY)
+        self.steps_done += 1
         # Convert to tensor
-        ob = torch.from_numpy(observation).float().unsqueeze(0)
+        ob = torch.from_numpy(observation).float().unsqueeze(0).to(self.device)
         # Switch to "evaluate mode"
-        self.network.eval()
+        self.net.eval()
 
         # With no gradient
         with torch.no_grad():
             # Calc Q Val for observation
-            qval = self.network(ob)
+            qval = self.net(ob)
 
         # Switch to "training mode"
-        self.network.train()   
+        self.net.train()   
 
         #Greedy
-        # Draw random in [0,1]
+        # Draw random in [0,1] 
         rand = random.random() 
-        if rand < (1 - epsilon):
+        if rand > eps_threshold:
             # Do best action
-            return numpy.argmax(qval.data.numpy())
+            return int(torch.argmax(qval))
         else:
             # Do random action : choose action in [0, |action_space|]
             return random.choice(numpy.arange(self.action_space.n))
+
+    def learn(self, sample_exp, gamma):
+        # Get (state, action, next_state, reward, endOfEp) of each interaction in the sample of experience
+        states = torch.from_numpy(numpy.vstack([exp.state for exp in sample_exp if exp is not None])).float().to(self.device)
+        actions = torch.from_numpy(numpy.vstack([exp.action for exp in sample_exp if exp is not None])).long().to(self.device)
+        next_states = torch.from_numpy(numpy.vstack([exp.next_state for exp in sample_exp if exp is not None])).float().to(self.device)
+        rewards = torch.from_numpy(numpy.vstack([exp.reward for exp in sample_exp if exp is not None])).float().to(self.device)
+        # .astype enable to convert bool -> int, cause we can't convert directly bool -> float
+        endsOfEp = torch.from_numpy(numpy.vstack([exp.endOfEp for exp in sample_exp if exp is not None]).astype(numpy.uint8)).float().to(self.device)
+
+        # Establish the loss function
+        loss_func = torch.nn.MSELoss()
+
+        self.target_net.eval()
+
+        prediction = self.net(states).gather(1, actions)
+        next_labels = self.net(next_states).max(1)[0].detach().unsqueeze(1)
+
+        # Bellman's equation
+        labels = rewards + (gamma * next_labels * (1 - endsOfEp))
+
+        loss = loss_func(prediction, labels)
+        self.optimizer.zero_grad()
+        loss.backward()
+        for param in self.net.parameters():
+            param.grad.data.clamp_(-1, 1)
+        self.optimizer.step()
+
+        if self.steps_done % 10000 == 0:
+            self.update_target()
+    
+    def update_target(self):
+        self.target_net.load_state_dict(self.net.state_dict())
